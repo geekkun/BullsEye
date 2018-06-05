51b7d7f8d6284792b7317fa8186250f411d0ad45
geekkun <alex.kuznetsov1@hotmail.com>
2018-06-05 05:29:20 +0100

    first

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var targetVal = 0
+    var score = 0
+    var round = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert(){
+        let diff = abs(targetVal-currentValue)
+        let points = 100 - diff
+        
+        score += points
+        round += 1
+        
+        let message = "You scored \(points)"
+        
+        let alert = UIAlertController(title: "Hello, world!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startRound()
+    }
+    
+    
+    func startRound(){
+        
+        targetVal = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetVal)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
 
 }
 
